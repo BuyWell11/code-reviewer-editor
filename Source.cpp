baa5e976221c5e96ce3db59cc514b1ab7f204e72
BuyWell <maksim-d2013@mail.ru>
2021-05-11 14:55:00 +0300

    Revert "Update Source.cpp"
    This reverts commit af8afd1d94d263b131c8209f9f84ddcc7c56b06b.

--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,6 @@
 #include "vars_initialization.h"
 #include "useless_fun.h"
 #include "incorrect_name.h"
-#include "formatting.h"
 #define SIZE 300
 
 typedef struct string_ {
@@ -17,122 +16,141 @@ typedef struct string_ {
 }strings;
 
 int main() {
+    FILE* input = fopen("main2.c", "r");
+    FILE* output = fopen("output.c", "w");
+    char buff[SIZE] = { 0 };
+    char c = '0';
+    char buff_char = '0';
     char recurs_funs[SIZE][SIZE] = { 0 };
     char vars[SIZE][SIZE] = { 0 };
     char vars_init_[SIZE][SIZE] = { 0 };
     char useless_funс[SIZE][SIZE] = { 0 };
     char incorrect_names[SIZE][SIZE] = { 0 };
+    int tabs = 0, cycle_deep = 0, max_deep = 0, tabs_flag = 0, comm_flag = 0, comm_end_flag = 1, cycle_flag = 0;
+
+    while (!feof(input)) {
+        fscanf(input, "%s", buff);
+        if (strcmp(buff, "}") == 0) {
+            tabs--;
+        }
+        for (int i = 0; i < tabs && tabs_flag == 1; i++) {
+            fprintf(output, "\t");
+        }
+        tabs_flag = 0;
+        c = fgetc(input);
+        if (comm_flag == 1) {
+            fprintf(output, "//");
+            comm_flag = 0;
+        }
+        if (strcmp(buff, "/*") && strcmp(buff, "*/")) {
+            fprintf(output, "%s", buff);
+        }
+        if ((c == ' ' || c == '\n') && strcmp(buff, "/*")) {
+            fprintf(output, "%c", c);
+            if (c == '\n') {
+                tabs_flag = 1;
+                if (comm_end_flag == 0) {
+                    comm_flag = 1;
+                }
+            }
+        }
+        if (buff[strlen(buff) - 1] == ';') {
+            if (c != '\n') {
+                fprintf(output, "\n");
+            }
+            tabs_flag = 1;
+        }
+        if (strcmp(buff, "}") == 0) {
+            if (tabs == cycle_deep) {
+                if (cycle_flag != 0) {
+                    cycle_deep--;
+                    cycle_flag--;
+                }
+            }
+            if (c != '\n') {
+                fprintf(output, "\n");
+            }
+            tabs_flag = 1;
+        }
+        if (strcmp(buff, "{") == 0) {
+            tabs++;
+            if (c != '\n') {
+                fprintf(output, "\n");
+            }
+            tabs_flag = 1;
+        }
+        if (strcmp(buff, "/*") == 0) {
+            comm_flag = 1;
+            comm_end_flag = 0;
+        }
+        if (strcmp(buff, "*/") == 0) {
+            comm_flag = 0;
+            comm_end_flag = 1;
+        }
+        if (strcmp(buff, "while") == 0 || strcmp(buff, "for") == 0) {
+            cycle_deep++;
+            cycle_flag++;
+            if (cycle_deep > max_deep) {
+                max_deep = cycle_deep;
+            }
+        }
+    }
+
+
+    ///////////////////////////////////////////
+    rewind(output);
     int cnt = 0;
-
-    ////////////////////////////////////////////// Максимальная вложенность циклов
-    cnt = formatting();
-
-    switch (cnt)
-    {
-    case 0:
-        printf("There are no cycles in the program.\n\n");
-        break;
-    default:
-        printf("Maximum cycle deep: %d.\n\n", cnt);
-    }
-
-    /////////////////////////////////////////// Зацикливание
-    cnt = looping();
-
-    switch (cnt)
-    {
-    case 1:
-        printf("Looping is here.\n\n");
-        break;
-    default:
-        printf("Looping isn't here.\n\n");
-    }
-
-    /////////////////////////////////////////// Неиспользуемые функции
-    cnt = useless_fun(useless_funс);
-
-    switch (cnt)
-    {
-    case 0:
-        printf("All functions are useful./There are no functions.\n\n");
-        break;
-    default:
-        printf("%d useless function(-s):\n", cnt);
-
-        for (int i = 0; i < cnt; i++) {
-            printf("%s\n", useless_funс[i]);
-        }
-        printf("\n");
-    }
-
-    /////////////////////////////////////////// Рекурсивные функции
     cnt = fun_cycle(recurs_funs);
+    printf("%d recursive function(-s):\n", cnt);
 
-    switch (cnt)
+    for (int k = 0; k < cnt; ++k)
     {
-    case 0:
-        printf("There are no recursive functions.\n\n");
-        break;
-    default:
-        printf("%d recursive function(-s):\n", cnt);
-
-        for (int k = 0; k < cnt; ++k)
-        {
-            printf("%s\n", recurs_funs[k]);
-        }
-        printf("\n");
+        printf("%s\n", recurs_funs[k]);
     }
-
-    /////////////////////////////////////////// Неиспользуемые переменные
+    ///////////////////////////////////////////
+    ///////////////////////////////////////////
     cnt = afk_var(vars);
 
-    switch (cnt)
+    printf("%d unused variable(-s):\n", cnt);
+    for (int k = 0; k < cnt; ++k)
     {
-    case 0:
-        printf("All variables are used./There are no variables.\n\n");
-        break;
-    default:
-        printf("%d unused variable(-s):\n", cnt);
-        for (int k = 0; k < cnt; ++k)
-        {
-            printf("%s\n", vars[k]);
-        }
-        printf("\n");
+        printf("%s\n", vars[k]);
     }
-
-    /////////////////////////////////////////// Начальные значения переменных
+    ///////////////////////////////////////////
     cnt = vars_init(vars_init_);
 
-    switch (cnt)
+    printf("%d variable(-s) aren't initialised:\n", cnt);
+    for (int k = 0; k < cnt; ++k)
     {
-    case 0:
-        printf("All variables have initial values./There are no variables.\n\n");
-        break;
-    default:
-        printf("%d variable(-s) isn't (aren't) initialised:\n", cnt);
-        for (int k = 0; k < cnt; ++k)
-        {
-            printf("%s\n", vars_init_[k]);
-        }
-        printf("\n");
+        printf("%s\n", vars_init_[k]);
     }
+    ///////////////////////////////////////////
+    cnt = looping();
 
-    ////////////////////////////////////////// Корректные имена переменных
+    if (cnt == 1)
+    {
+        printf("Looping is here\n");
+    }
+    else
+    {
+        printf("Looping isn't here\n");
+    }
+    ///////////////////////////////////////////
+    cnt = useless_fun(useless_funс);
+    printf("Useless func:\n");
+    for (int i = 0; i < cnt; i++) {
+        printf("%s\n", useless_funс[i]);
+    }
+    //////////////////////////////////////////////
+    printf("Maximum cycle deep: %d\n", max_deep);
+    //////////////////////////////////////////
     cnt = incorrect_name(incorrect_names);
-
-    switch (cnt)
-    {
-    case 0:
-        printf("All variables have correct names./There are no variables.\n\n");
-        break;
-    default:
-        printf("Incorrect names:\n");
-        for (int i = 0; i < cnt; i++)
-        {
-            printf("%s\n", incorrect_names[i]);
-        }
-        printf("\n");
+    printf("Incorrect names:\n");
+    for (int i = 0; i < cnt; i++) {
+        printf("%s\n", incorrect_names[i]);
     }
 
+    fclose(input);
+    fclose(output);
     return 0;
 }
